972ae233f0104dd99b6bbbdaf8e5d67501bd6705
Zerbo <raphael23@schule.bremen.de>
2023-01-20 22:26:31 +0100

    Add LogIn-Function

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Observable, of} from "rxjs";
 import {Employee} from "../Employee";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {KeycloakService} from "keycloak-angular";
 
 @Component({
   selector: 'app-employee-list',
@@ -10,12 +11,17 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 })
 export class EmployeeListComponent {
 
-  bearer = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIzUFQ0dldiNno5MnlQWk1EWnBqT1U0RjFVN0lwNi1ELUlqQWVGczJPbGU0In0.eyJleHAiOjE2NzQyNDg5MDQsImlhdCI6MTY3NDI0NTMwNCwianRpIjoiZTUxYzU5OTktZjZiMi00Mzk2LTk4ZGYtY2JhYjhhNmUwNjEwIiwiaXNzIjoiaHR0cHM6Ly9rZXljbG9hay5zenV0LmRldi9hdXRoL3JlYWxtcy9zenV0IiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjU1NDZjZDIxLTk4NTQtNDMyZi1hNDY3LTRkZTNlZWRmNTg4OSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImVtcGxveWVlLW1hbmFnZW1lbnQtc2VydmljZSIsInNlc3Npb25fc3RhdGUiOiJjNzM0ODBkNS1hNzkwLTQzY2MtYTVlNy1jN2NkMDFkZjZlMGQiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwiZGVmYXVsdC1yb2xlcy1zenV0IiwidW1hX2F1dGhvcml6YXRpb24iLCJ1c2VyIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInByZWZlcnJlZF91c2VybmFtZSI6InVzZXIifQ.OUPOvFBozTo3t9M-FoVjw55lT0mJrdizHi-1cBe_rjTQEQTDVIKEX1CyrLNgE22lZ-4BGzMk10OUU-I-mIwErQyElAOu5VkdMDnc7dcAj-QOXR-LWqtH2gSTAnioJX3p2rcFqwkUnJRx6NLYjCXCqxSZ5PabI8kwHSTxTGKLPIqljNSEVVLhy7vh0_gD6pDKC9kTcKtB3ibPVTmtxg9GJThW2HZxa0xOJJyK8_gCaUi5HZWLysJoRdeR9ZEX_kbOF7hgovoj1fNEmjmyBHDkRgqnc7K3KqtvEaPKAyyO1oAIYKNYNI49L3gjFIJDqIWD-1IiHvxH1l241EMzE19OzA';
+  bearer = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIzUFQ0dldiNno5MnlQWk1EWnBqT1U0RjFVN0lwNi1ELUlqQWVGczJPbGU0In0.eyJleHAiOjE2NzQyNTE0NzcsImlhdCI6MTY3NDI0Nzg3NywianRpIjoiNTE5ZDVhMjctMjQ3Yy00Y2RmLThkZDUtZDNmZDQ5MWRjYjU2IiwiaXNzIjoiaHR0cHM6Ly9rZXljbG9hay5zenV0LmRldi9hdXRoL3JlYWxtcy9zenV0IiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjU1NDZjZDIxLTk4NTQtNDMyZi1hNDY3LTRkZTNlZWRmNTg4OSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImVtcGxveWVlLW1hbmFnZW1lbnQtc2VydmljZSIsInNlc3Npb25fc3RhdGUiOiI4YjI1ZTExYy0zOWQxLTQzMWEtODg4Mi0xY2QyNzViZTVmNmUiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwiZGVmYXVsdC1yb2xlcy1zenV0IiwidW1hX2F1dGhvcml6YXRpb24iLCJ1c2VyIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInByZWZlcnJlZF91c2VybmFtZSI6InVzZXIifQ.eMdtQfpexnR35Byyg1tdvWMRhBYIN-fhF-AEnKfaQWU2IlfYrfX5o-RCC8AgH7aPOizkvijCQL8d9pbdWauo5eyG8Nhu9XF1XJPl4qbaDYEMASsD_SXJlgJ-G-8jLeSL_g7AkchAXiizQWgWIsgYSpNs4-BJz_t5JH6XQeiud84Si41NJ2jGJxbishzdoDle0nnDVAQ5U3ABMNm2Rd6nzGkgcj9iVOMYxiOswPKzok89lxTsG-ZjeoHJzIacAuQNkRkioZtIJutdYUG1zjyHnyh1mWNZwOYFCQ0H-_hTF3m3i5bQrMmVfC-kAXjPwDNqiJCdXcufBSqkY2s5In4xpw';
   employees$: Observable<Employee[]>;
 
-  constructor(private http: HttpClient) {
+  user = "";
+  constructor(private http: HttpClient, private keycloakService: KeycloakService) {
     this.employees$ = of([]);
     this.fetchData();
+  }
+
+  ngOnInit(): void{
+    this.initializeUserOptions();
   }
 
   fetchData() {
@@ -24,6 +30,14 @@ export class EmployeeListComponent {
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${this.bearer}`)
     });
+  }
+
+  private initializeUserOptions(): void{
+    this.user = this.keycloakService.getUsername();
+  }
+
+  logout(): void{
+    this.keycloakService.logout();
   }
 
 }
