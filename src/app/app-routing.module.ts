a59c9a6f9932f80ef79e7699ed3889c1d2e90dac
Zerbo <raphael23@schule.bremen.de>
2023-01-20 22:26:31 +0100

    Add LogIn-Function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import{EmployeeListComponent} from "./employee-list/employee-list.component";
 import {AuthGuard} from "./utility/app.guard";
-import {EditEmployeeComponent} from "./edit-employee/edit-employee.component";
-import {AddEmployeeComponent} from "./add-employee/add-employee.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/employee', pathMatch: 'full' },
   { path: 'employee', component: EmployeeListComponent, canActivate: [AuthGuard] },
-  { path: 'employee-edit/:id', component: EditEmployeeComponent,  canActivate: [AuthGuard] },
-  { path: 'employee-add', component: AddEmployeeComponent,  canActivate: [AuthGuard] }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,28 +7,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
 import {initializeKeycloak} from "./utility/app.init";
-import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
-import { HeaderComponent } from './header/header.component';
-import {FormsModule} from "@angular/forms";
-import { AddEmployeeComponent } from './add-employee/add-employee.component';
-import { SkillForFormsComponent } from './skill-for-forms/skill-for-forms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent,
-    EditEmployeeComponent,
-    HeaderComponent,
-    AddEmployeeComponent,
-    SkillForFormsComponent
+    EmployeeListComponent
   ],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        AppRoutingModule,
-        KeycloakAngularModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    KeycloakAngularModule
+  ],
   providers: [
     {
       provide: APP_INITIALIZER,
