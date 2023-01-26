2cc4ab0fe3e2d1331a0b3f277c75ffdf28927cb8
Zerbo <raphael23@schule.bremen.de>
2023-01-26 11:48:01 +0100

    Add Header and edit-employee component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import{EmployeeListComponent} from "./employee-list/employee-list.component";
 import {AuthGuard} from "./utility/app.guard";
+import {EditEmployeeComponent} from "./edit-employee/edit-employee.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/employee', pathMatch: 'full' },
   { path: 'employee', component: EmployeeListComponent, canActivate: [AuthGuard] },
+  { path: 'employee-edit', component: EditEmployeeComponent }
 ];
 
 @NgModule({
