b2aac7c2f063f9bdd791c9dbbc3a14d756b2af77
nspohd <norman.spohd@ato.de>
2023-02-04 17:36:03 +0100

    Routing changes and folder structure optimization

---
+++ src/app/edit-qualification/edit-qualification.component.spec.ts
@@ -0,0 +1,24 @@
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {EditQualificationComponent} from "./edit-qualification.component";
+
+describe('EditQualificationComponent', () => {
+  let component: EditQualificationComponent;
+  let fixture: ComponentFixture<EditQualificationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EditQualificationComponent ]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EditQualificationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
