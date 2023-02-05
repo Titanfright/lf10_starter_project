7a2478eb9371282ecbcd451b954d428d1b1d79da
nspohd <norman.spohd@ato.de>
2023-02-05 13:34:59 +0100

    update

--- src/app/add-qualification/add-qualification.component.ts
+++ src/app/add-qualification/add-qualification.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
 import {QualificationApiService} from "../QualificationApiService";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-add-qualification',
@@ -14,17 +15,22 @@ export class AddQualificationComponent implements OnInit {
   @Input() qualificationsDetails = {qualification: ''};
 
   constructor(public router: Router,
-              public callApiService: QualificationApiService) {}
+              public callApiService: QualificationApiService,
+              public httpClient: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.httpClient.get('http://localhost:4200/header', {observe: "response"})
+      .subscribe(data => {
+        console.log(data);
+    })
+  }
 
   addQualification(dataQualification: any) {
     this.callApiService.createQualification(this.qualificationsDetails)
       .subscribe((data: {}) => {
-        this.router.navigate(['/list-qualification']);
+        this.router.navigate(['/add-qualification']);
       });
   }
-
   /*onSubmit() {
     this.callApiService.createQualification(this.addQualification)
       .subscribe( data => {
