2ec544910bb81616f33bbf48d3510e413cd9e4ef
nspohd <norman.spohd@ato.de>
2023-02-08 23:57:11 +0100

    Filter doesn't work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,43 @@ import {AddQualificationComponent} from "./add-qualification/add-qualification.c
 import {HeaderComponent} from "./header/header.component";
 import {ListQualificationComponent} from "./list-qualification/list-qualification.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {Qualification} from "./Qualification";
 import { EditQualificationComponent } from './edit-qualification/edit-qualification.component';
-import {TokenInterceptorService} from "../token-interceptor.service";
 import { FilterComponent } from './filter/filter.component';
-import { FilterPipe } from './filter.pipe';
-import {Ng2SearchPipeModule} from "ng2-search-filter";
+import {MatNativeDateModule, MatRippleModule} from "@angular/material/core";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatRadioModule} from "@angular/material/radio";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatInputModule} from "@angular/material/input";
+import {MatListModule} from "@angular/material/list";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {MatBadgeModule} from "@angular/material/badge";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatCardModule} from "@angular/material/card";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatBottomSheetModule} from "@angular/material/bottom-sheet";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSelectModule} from "@angular/material/select";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatSortModule} from "@angular/material/sort";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatStepperModule} from "@angular/material/stepper";
+import {MatTableModule} from "@angular/material/table";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatTreeModule} from "@angular/material/tree";
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 export function kcFacotry(kcSecService: KeycloakService) {
   return () => kcSecService.init();
@@ -31,8 +62,6 @@ export function kcFacotry(kcSecService: KeycloakService) {
     ListQualificationComponent,
     EditQualificationComponent,
     FilterComponent,
-    FilterPipe,
-    //SearchPipe
   ],
   imports: [
     BrowserModule,
@@ -40,7 +69,44 @@ export function kcFacotry(kcSecService: KeycloakService) {
     AppRoutingModule,
     KeycloakAngularModule,
     FormsModule,
+    MatNativeDateModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
   ],
   providers: [
     {
